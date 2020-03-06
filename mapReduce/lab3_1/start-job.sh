fa635534e7a131d5cc191193ed7f0e5f67326272
Alexey R <alexey_r.98@ngs.ru>
2020-03-06 12:14:45 +0700

    -

--- mapReduce/lab3_1/start-job.sh
+++ mapReduce/lab3_1/start-job.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
-hadoop jar ./bigramcount.jar mapred.bigram.BigramCount -D mapreduce.job.reduces=4 /pub/etwiki.xml bigram/bg ./skip.en
+hdfs dfs -rm -r -f ./bigram/output
+#hadoop jar ./bigramcount.jar mapred.bigram.BigramCount -D mapreduce.job.reduces=4 /pub/etwiki.xml ./bigram/bg ./skip.en 
+hadoop jar ./bigramcount.jar mapred.bigram.BigramCount -D mapreduce.job.reduces=4 /pub/ruwiki.xml ./bigram/output ./skip.ru:
 
 
 
