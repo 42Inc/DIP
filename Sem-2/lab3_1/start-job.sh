bd62c794d5fe28c9c952ab3aeb7478c8fdcbacfd
Alexey R <alexey_r.98@ngs.ru>
2020-03-06 12:29:14 +0700

    :

--- Sem-2/lab3_1/start-job.sh
+++ Sem-2/lab3_1/start-job.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 hdfs dfs -rm -r -f ./bigram/output
 #hadoop jar ./bigramcount.jar mapred.bigram.BigramCount -D mapreduce.job.reduces=4 /pub/etwiki.xml ./bigram/bg ./skip.en 
-hadoop jar ./bigramcount.jar mapred.bigram.BigramCount -D mapreduce.job.reduces=4 /pub/ruwiki.xml ./bigram/output ./skip.ru:
+hadoop jar ./bigramcount.jar mapred.bigram.BigramCount -D mapreduce.job.reduces=4 /pub/ruwiki.xml ./bigram/output ./skip.ru
 
 
 
