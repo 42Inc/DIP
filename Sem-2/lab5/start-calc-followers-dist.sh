12aabe666aac9a67984f8e42f844741f776c73f2
Alexey R <alexey_r.98@ngs.ru>
2020-03-28 17:51:33 +0700

    man

--- Sem-2/lab5/start-calc-followers-dist.sh
+++ Sem-2/lab5/start-calc-followers-dist.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if [ ! $# -eq 1 ]; then
-  printf "Usage: ./start-calc-followers-count.sh <REDUCER COUNT>\n"
+  printf "Usage: ./start-calc-followers-dist.sh <REDUCER COUNT>\n"
   exit 1
 fi
 
