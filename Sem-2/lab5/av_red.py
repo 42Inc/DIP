3d371c08dc17250740f9bdec92bea55ad3241150
Alexey R <alexey_r.98@ngs.ru>
2020-03-28 19:07:53 +0700

    Complete average task

---
+++ Sem-2/lab5/av_red.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import sys
+
+if __name__ == "__main__":
+    users_count = 0
+    followers_count = 0.0
+    average = 0.0
+    for line in sys.stdin:
+	data = line.split("\t")
+	value = data[1]
+	users_count += 1;
+	followers_count = followers_count + int(value)
+    if users_count != 0:
+	average = followers_count / users_count
+    print '%s\t%s' % (users_count, average)
